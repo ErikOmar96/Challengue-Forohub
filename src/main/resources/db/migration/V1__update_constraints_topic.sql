04722f216f0814302e4a601e3c33ee7ec4dbb566
OscarDevCPP <oscarchh0799@gmail.com>
2024-07-13 01:22:39 -0500

    modify migration V1

--- src/main/resources/db/migration/V1__update_constraints_topic.sql
+++ src/main/resources/db/migration/V1__update_constraints_topic.sql
@@ -6,5 +6,3 @@ ALTER TABLE topic
     MODIFY created_at DATETIME(6) NOT NULL;
 ALTER TABLE topic
     MODIFY status enum('DELETED', 'RESOLVED', 'PENDING') NOT NULL;
-ALTER TABLE topic
-    ADD CONSTRAINT uc_title UNIQUE (title);
